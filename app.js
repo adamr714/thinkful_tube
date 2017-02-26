05bb080c326a0ca0de0811062e83a37eec13b3f7
Adam Rosenblum <AdamR714@gmail.comgit config --global user.emailgit config --global user.email AdamR714@gmail.com>
2017-02-25 21:00:11 -0800

    added more interactivity and fixed search results

--- app.js
+++ app.js
@@ -10,30 +10,41 @@
 
 var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
+
 function getDataFromApi(searchTerm, callback) {
   var settings = {
     url: YOUTUBE_BASE_URL,
     data: {
-      s: searchTerm,
-      r: 'json',
+      // s: searchTerm,
+      // r: 'json',
       key: 'AIzaSyAUDNuTfHa5XAwGIdS_vYZxyJEQG19grm8',
       part: 'snippet',
-      q: 'searchTerm',
+      q: searchTerm,
     },
     dataType: 'json',
     type: 'GET',
-    success: callback
+    success: callback,
+    error: callbackError
   };
   $.ajax(settings);
 }
 
+function callbackError(jqXHR, textStatus, errorThrown ) {
+	console.log(textStatus);
+	console.log(errorThrown);
+}
 
 
 function displayYouTubeSearchData(data) {
-  var resultElement = '';
-  if (data.Search) {
-    data.Search.forEach(function(item) {
-     resultElement += '<p>' + items + '</p>';
+  var resultElement = '<div class="whatever">';
+  if (data.items) {
+    data.items.forEach(function(item) {
+     resultElement += '<h2>' + item.snippet.title + '</h2>';
+     resultElement += '<p>' + item.snippet.description + '</p>';
+     resultElement += '</div>';
+
+     // resultElement += '<p>' + item.snippet.thumnbnails.medium.url + '</p>';
+     
     });
   }
   else {
@@ -42,6 +53,7 @@ function displayYouTubeSearchData(data) {
   
   $('.js-search-results').html(resultElement);
 }
+
 
 function watchSubmit() {
   $('.js-search-form').submit(function(event) {
