c6ad6e25749db2160c722b18bd7746dc2b826358
Adam Rosenblum <AdamR714@gmail.comgit config --global user.emailgit config --global user.email AdamR714@gmail.com>
2017-02-26 20:06:43 -0800

    updated everything

--- app.js
+++ app.js
@@ -1,73 +1,52 @@
-
-
-
-
-// var apiKey = "AIzaSyAUDNuTfHa5XAwGIdS_vYZxyJEQG19grm8"
-
-
-
-
-
-var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
-
-
-function getDataFromApi(searchTerm, callback) {
-  var settings = {
-    url: YOUTUBE_BASE_URL,
-    data: {
-      // s: searchTerm,
-      // r: 'json',
-      key: 'AIzaSyAUDNuTfHa5XAwGIdS_vYZxyJEQG19grm8',
-      part: 'snippet',
-      q: searchTerm,
-    },
-    dataType: 'json',
-    type: 'GET',
-    success: callback,
-    error: callbackError
-  };
-  $.ajax(settings);
-}
-
-function callbackError(jqXHR, textStatus, errorThrown ) {
-	console.log(textStatus);
-	console.log(errorThrown);
-}
-
-
-function displayYouTubeSearchData(data) {
-  var resultElement = '<div class="whatever">';
-  if (data.items) {
-    data.items.forEach(function(item) {
-     resultElement += '<h2>' + item.snippet.title + '</h2>';
-     resultElement += '<p>' + item.snippet.description + '</p>';
-     resultElement += '</div>';
-
-     // resultElement += '<p>' + item.snippet.thumnbnails.medium.url + '</p>';
-     
-    });
-  }
-  else {
-    resultElement += '<p>No results</p>';
-  }
-  
-  $('.js-search-results').html(resultElement);
-}
-
-
-function watchSubmit() {
-  $('.js-search-form').submit(function(event) {
-    event.preventDefault();
-    var query = $(this).find('.js-query').val();
-    console.log(query);
-    getDataFromApi(query, displayYouTubeSearchData);
-
-  });
-}
-
-// $(function(){watchSubmit();});
-
 $(document).ready(function() {
-	watchSubmit();
+    // STEP 1 - Get User Input
+    $("#search-form").submit(function (event) {
+        event.preventDefault();
+        var userInput = $("#query").val();
+        getResults(userInput);
+    });
+
+    // STEP 2 - Use Input to Make API Call
+    function getResults(userInput) {
+        $.getJSON("https://www.googleapis.com/youtube/v3/search", {
+                part: 'snippet', 
+                maxResults: 6, 
+                key: 'AIzaSyAUDNuTfHa5XAwGIdS_vYZxyJEQG19grm8',
+                q: userInput, 
+                type: 'video' 
+            },
+
+            function (receivedApiData) {
+                console.log(receivedApiData);
+                if (receivedApiData.pageInfo.totalResults == 0) {
+                    alert("No videos found!");
+                }
+                else {
+                    displaySearchResults(receivedApiData.items);
+                }
+            });
+    }
+
+    // STEP 3 - Prepare Results
+    function displaySearchResults(videosArray) {
+        var results = '';
+        $.each(videosArray, function (videosArrayKey, videosArrayValue) {
+            results += "<div class='col-4 box'>"; 
+            results += "<a href='https://www.youtube.com/watch?v=" + videosArrayValue.id.videoId + "' target='_blank'>"; //Link to video
+            results += "<img src='" + videosArrayValue.snippet.thumbnails.medium.url + "'/>"; //Thumbnail
+            results += "</a>";
+            results += "<h2>" + videosArrayValue.snippet.title + "</h2>"; //Video Title
+            results += "</div>";
+        });
+
+    //Step 4 - Show the Results
+        $("#search-results").html(results);
+    }
 });
+
+
+
+
+
+
 
